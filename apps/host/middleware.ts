77152f3cbe5e1ba188e3ccbeceaab2f0e4d49573
Mate Hegedus <matehegedus.mh@gmail.com>
2026-04-03 11:11:05 +0200

    explained middleware matcher

--- apps/host/middleware.ts
+++ apps/host/middleware.ts
@@ -4,6 +4,8 @@ export default createMiddleware({
   defaultLocale: "en",
 });
 
+// without this middleware would run on JS bundles and would inject /en (localization) before the path
+// so we would not get the JS -> no hydration
 export const config = {
   // Only run middleware on page routes, skip Next.js internals and static files
   matcher: ["/((?!_next|api|favicon.ico|.*\\..*).*)"],
